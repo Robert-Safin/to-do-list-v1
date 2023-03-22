0e3ea2e6d4bbb7e1dd58335256da650684d2a5b9
Robert Safin <robertsafin98@gmail.com>
2023-03-22 10:51:22 +0800

    middleware

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+//import dependencies
 import express from 'express';
+import bodyParser from 'body-parser';
 import fetch from 'node-fetch';
 
+// create app
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
+// connect root
 app.get('/', (req, res) => {
   res.send("hello")
 })
