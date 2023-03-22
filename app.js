278b243f002d688c5f71fe305c257e9fa0012e86
Robert Safin <robertsafin98@gmail.com>
2023-03-22 11:25:03 +0800

    index page + middleware

--- app.js
+++ app.js
@@ -2,18 +2,23 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import fetch from 'node-fetch';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
 // create app
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+
 // connect root
 app.get('/', (req, res) => {
-  res.send("hello")
+  res.sendFile(__dirname + "/index.html");
 })
 
 
