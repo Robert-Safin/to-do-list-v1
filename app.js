3e30c8cd449d2c1984e3ec57767cf366a40afb4e
Robert Safin <robertsafin98@gmail.com>
2023-03-22 12:05:16 +0800

    ejs implemented

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //import dependencies
-import express from 'express';
-import bodyParser from 'body-parser';
-import fetch from 'node-fetch';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
+import express from "express";
+import bodyParser from "body-parser";
+import fetch from "node-fetch";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
 // create app
 const app = express();
@@ -13,16 +13,44 @@ const __dirname = dirname(__filename);
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
+let day = new Date().getDay()
 
 // connect root
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-})
+app.get("/", (req, res) => {
+  switch (day) {
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    case 0:
+      day = "Sunday";
+      break;
 
+    default:
+      break;
+  }
+
+  res.render("list", { day: day });
+});
 
 // Start the server
 app.listen(3000, () => {
-  console.log('Server started on port 3000...');
+  console.log("Server started on port 3000...");
 });
