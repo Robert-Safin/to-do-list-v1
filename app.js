479eaf765a6afdaddf9f71ef014c058f8554a41a
Robert Safin <robertsafin98@gmail.com>
2023-03-27 16:33:36 +0800

    live db, .gitignore

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 import getDay from "./date.js";
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+const mongoPassword = process.env.MONGO_PASSWORD;
 
 // create app
 const app = express();
@@ -18,29 +21,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-
-
-
-
-
-
-
-connectToDb()
+connectToDb();
 
 async function connectToDb() {
   try {
-  await mongoose.connect('mongodb://127.0.0.1:27017/toDoList', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      console.log('connected to DB');
-    } catch(error) {
-      console.log(error);
-    }
+    await mongoose.connect(`mongodb+srv://admin:${mongoPassword}@cluster0.ruawe0b.mongodb.net/?retryWrites=true&w=majority`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("connected to DB");
+  } catch (error) {
+    console.log(error);
+  }
 }
-
-
-
 
 const toDoSchema = new mongoose.Schema({
   name: String,
@@ -51,56 +44,40 @@ const ToDo = mongoose.model("ToDo", toDoSchema);
 async function saveTip() {
   const items = await ToDo.find({});
   if (items.length === 0) {
-    await ToDo.create({name:"To Do List"});
+    await ToDo.create({ name: "To Do List" });
   }
 }
-
 saveTip();
 
-
-
-
-
-
-
-
-
-
-
-
-app.get("/", async(req, res) => {
+app.get("/", async (req, res) => {
   try {
-    const toDos = await ToDo.find({})
+    const toDos = await ToDo.find({});
     res.render("list", { listTitle: "Today", toDos: toDos });
-  } catch(error) {
+  } catch (error) {
     console.log(error);
   }
 });
 
-app.post("/", (req, res) => {
-  const toDo = new ToDo({
-    name: req.body.toDo,
-  });
-  toDo.save()
+app.post("/", async(req, res) => {
+
+  try {
+    await ToDo.create({ name: req.body.toDo })
     res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
+
 });
 
-app.post('/delete', async (req, res) => {
-  const itemId = req.body.checkbox.trim()
+app.post("/delete", async (req, res) => {
+  const itemId = req.body.checkbox.trim();
   try {
-    await ToDo.deleteOne({_id:itemId})
+    await ToDo.deleteOne({ _id: itemId });
   } catch (error) {
     console.log(error);
   }
   res.redirect("/");
 });
-
-
-
-
-
-
-
 
 // Start the server
 app.listen(3000, () => {
